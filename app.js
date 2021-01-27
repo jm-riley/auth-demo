5621e465b39da0d52bc0f7afffc300685491ab1f
John-Michael Riley <johnmichael.w.riley@gmail.com>
2021-01-26 23:15:46 -0500

    initial commit :)

---
+++ app.js
@@ -0,0 +1,46 @@
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('view engine', 'pug');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(logger('dev'));
+
+// set up session middleware
+app.use(
+  session({
+    secret: 'superSecret',
+    saveUninitialized: false,
+    resave: false,
+  })
+);
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const csrf = require('csurf');
+const { User } = require('../db/models');
+const router = express.Router();
+
+const csrfProtection = csrf({ cookie: true });
+
+// 1. GET route that renders a register form
+
+// 2. POST route to register a new user
+
+// 3. GET route that renders a login form
+
+// 4. POST route to handle user login
+module.exports = router;
