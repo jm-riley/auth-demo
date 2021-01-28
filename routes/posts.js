8bf77bf1227ae3b0ab41927e023abff82c0c28e2
mitchellreiss <mitchellreiss@gmail.com>
2021-01-27 22:41:49 -0800

    Add /posts/new route and templates

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const { Post, User } = require('../db/models');
-const { asyncHandler } = require('./util');
+const { asyncHandler, csrfProtection } = require("./util");
 
 router.get(
-  '/',
+  "/",
   asyncHandler(async (req, res) => {
     const posts = await Post.findAll({ include: User });
-    res.render('posts', { posts });
+    res.render("posts", { posts });
+  })
+);
+
+router.get(
+  "/new",
+  csrfProtection,
+  asyncHandler(async (req, res) => {
+    res.render("post-form", { csrfToken: req.csrfToken() });
+  })
+);
+
+router.post(
+  "/",
+  csrfProtection,
+  asyncHandler(async (req, res) => {
+    const { content } = req.body;
+    console.log(res.locals.user);
+    await Post.create({ content, userId: res.locals.user.id });
+    res.redirect("/posts");
   })
 );
 
